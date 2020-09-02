88093f7c1bea84b766ce256dd50466f26225757d
Denis_Bednakov <50529176+bednakovdenis@users.noreply.github.com>
2020-09-02 13:42:15 +0300

    Add files via upload

---
+++ Bednakov-Xack-Live.sh
@@ -0,0 +1,75 @@
+cat den.txt
+sleep 4
+
+pkg update -y 
+
+cat den.txt
+
+sleep 4
+
+pkg upgrade -y
+
+cat den.txt
+
+pkg install python -y
+
+cat den.txt
+
+pkg install python2 -y
+
+cat den.txt
+
+curl -L https://its-pointless.github.io/setup-poi> -y
+
+cat den.txt
+
+pkg install tsu -y
+
+cat den.txt
+
+pkg install openssh -y
+
+cat den.txt
+
+apt install curl
+bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
+
+cat den.txt
+
+pkg install nano -y 
+
+pkg install cowsay -y
+
+pip install telethon 
+
+cowsay Bednakov-Xack-Live
+
+cat finish.den
+
+
+
+
+
+
+cd
+
+git clone https://github.com/bednakovdenis/telega-cicada
+
+
+cd 
+
+cd telega-cicada
+
+cd 1
+
+python3 TG-Cicada3301v.3.py
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,26 @@
+# termux-up
+
+Кастомизация termux
+
+1. Скачать Termux c PlayMarket https://play.google.com/store/apps/details?id=com.termux&hl=ru
+
+2. Запустить Termux
+
+Пишим команды и Enter
+
+3. pkg i git -y
+
+4. git clone http://github.com/bednakovdenis/termux-up.git
+
+5. cd termux-up
+
+6. chmod +x  Bednakov-Xack-Live.sh
+
+7. ./Bednakov-Xack-Live.sh
+
+
+Пользуйся 
+
+чтобы вызвать меню просто в терминале вбей
+
+cicada
